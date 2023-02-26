a582cbc5ba7d97e2ac1989f227aa808b29c23c2a
light4 <root@i01.io>
2023-02-26 18:04:27 +0800

    update elm-test

--- completions/elm-test.fish
+++ completions/elm-test.fish
@@ -2,7 +2,7 @@ complete -c elm-test -n "__fish_use_subcommand" -s h -l help -d 'Prints help inf
 complete -c elm-test -n "__fish_use_subcommand" -l watch -d 'Run tests on file changes (default: false)'
 complete -c elm-test -n "__fish_use_subcommand" -l seed -d '<int>  Run with a specific fuzzer seed (default: random)'
 complete -c elm-test -n "__fish_use_subcommand" -l fuzz -d '<int>  Define how many times each fuzz-test should run (default: 100)'
-complete -c elm-test -n "__fish_use_subcommand" -l report -d '<format>  Specify which format to use for reporting test results (choices: "json", "junit", "console", default: "console")'
+complete -c elm-test -n "__fish_use_subcommand" -l report -a 'json junit console' -d '<format>  Specify which format to use for reporting test results (choices: "json", "junit", "console", default: "console")'
 complete -c elm-test -n "__fish_use_subcommand" -l no-color -d 'Disable colored console output (setting FORCE_COLOR=0 also works)'
 complete -c elm-test -n "__fish_use_subcommand" -l color -d 'Force colored console output (setting FORCE_COLOR to anything but 0 also works)'
 complete -c elm-test -n "__fish_use_subcommand" -l compiler -d '<path>  Use a custom path to an Elm executable (default: elm)'
@@ -12,7 +12,3 @@ complete -c elm-test -n "__fish_use_subcommand" -f -a "init" -d 'Install elm-exp
 complete -c elm-test -n "__fish_use_subcommand" -f -a "install" -d 'Like `elm install package`, except it installs to "test-dependencies" in your elm.json'
 complete -c elm-test -n "__fish_use_subcommand" -f -a "make" -d 'Check files matching the globs for compilation errors'
 complete -c elm-test -n "__fish_use_subcommand" -f -a "help" -d 'Show help'
-
-complete -c elm-test -n "__fish_seen_subcommand_from --report" -f -a "json" -d 'use json for reporting test results'
-complete -c elm-test -n "__fish_seen_subcommand_from --report" -f -a "junit" -d 'use junit for reporting test results'
-complete -c elm-test -n "__fish_seen_subcommand_from --report" -f -a "console" -d 'use console for reporting test results'
