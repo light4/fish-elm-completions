aeaa0239b71c4299c8863c1d3b588469e786818d
light4 <root@i01.io>
2023-02-26 17:45:35 +0800

    add elm-format

---
+++ completions/elm-format.fish
@@ -0,0 +1,6 @@
+complete -c elm-format -n "__fish_use_subcommand" -s h -l help -d 'Prints help information'
+complete -c elm-format -n "__fish_use_subcommand" -l output -d 'Write output to FILE instead of overwriting the given source file.'
+complete -c elm-format -n "__fish_use_subcommand" -l yes -d "Reply 'yes' to all automated prompts."
+complete -c elm-format -n "__fish_use_subcommand" -l validate -d "Check if files are formatted without changing them."
+complete -c elm-format -n "__fish_use_subcommand" -l stdin -d "Read from stdin, output to stdout."
+complete -c elm-format -n "__fish_use_subcommand" -l elm-version -d "VERSION The Elm version of the source files being formatted"
